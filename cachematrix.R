b6a7afc4da32692231082283af8d911335ae40ff
Zacktao <44105198+Zacktao@users.noreply.github.com>
2018-10-28 01:57:38 -0500

    Add files via upload
    ## the function read the value of inverse from the list 
    ## if there is no such thing, it calculates the inverse and set it to the list
    ## if there is one from the calculation in the past, it returns the inverse

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## the function creat a list that contains four functions, which are set, get, setinverse,getinverse. 
+## and these function will set the value and get the value of input, and set the value and get the value of inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  INV<- NULL
+  set<- function(y){
+    x<<-y
+    INV<- NULL
+  }
+  get<- function(){x}
+  setInverse<-function(d){INV <<-d}
+  getInverse<- function(){INV}
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+## the function read the value of inverse from the list 
+## if there is no such thing, it calculates the inverse and set it to the list
+## if there is one from the calculation in the past, it returns the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  INV <- x$getInverse()
+  if(!is.null(INV)){
+    message("getting cached data")
+    return(INV)
+  }
+  mdata<-x$get()
+  Inverse<- solve(mdata)
+  x$setInverse(Inverse)
+  Inverse
 }
